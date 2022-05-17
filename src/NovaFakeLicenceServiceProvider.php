45390ad5487456f274405f5c6e96428c7f94e4f5
nova-lol <104551277+nova-lol@users.noreply.github.com>
2022-05-17 14:17:02 +0500

    Add fake string.

--- src/NovaFakeLicenceServiceProvider.php
+++ src/NovaFakeLicenceServiceProvider.php
@@ -4,11 +4,13 @@ namespace NovaLol\FakeLicense;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Config;
 
 class NovaFakeLicenceServiceProvider extends ServiceProvider
 {
     public function register()
     {
+        Config::set('nova.license_key', 'this.is.fake.licence');
         Http::fake(['nova.laravel.com/*' => Http::response(['success' => true], 204, [])]);
     }
 }
